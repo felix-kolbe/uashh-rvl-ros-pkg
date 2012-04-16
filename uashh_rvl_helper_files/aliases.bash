af556b63bb961ce81683e7887607234b74161950
Felix Kolbe <felix.kolbe@informatik.haw-hamburg.de>
2012-04-16 17:36:38 +0200

    aliases_reload and joint_ref_gripper

--- uashh_rvl_helper_files/aliases.bash
+++ uashh_rvl_helper_files/aliases.bash
@@ -1,9 +1,11 @@
 HELPER_PKG=uashh_rvl_helper_files
 
 alias aliases_help='rosed '$HELPER_PKG' aliases.bash'
+alias aliases_reload='source `rospack find $HELPER_PKG`/aliases.bash'
+
 
 # config adjustments
-#alias ros_what='echo ROS_MASTER_URI=$ROS_MASTER_URI; echo ROS_ROOT=$ROS_ROOT; echo ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH'
+alias ros_what='echo ROS_MASTER_URI=$ROS_MASTER_URI; echo ROS_ROOT=$ROS_ROOT; echo ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH'
 alias ros_scitos='export ROS_MASTER_URI=http://scitos_w:11311'
 alias ros_local='export ROS_MASTER_URI=http://localhost:11311'
 
@@ -15,9 +17,11 @@ alias rs='roslaunch rotoscan_node laserscanner_w_tf.launch'
 alias cam='roslaunch camera1394 haw_cam.launch'
 alias kinect='roslaunch '$HELPER_PKG' openni_with_calibration_haw.launch'
 
+
 # helper nodes
 alias jms='rosrun joint_motion_service joint_motion_service'
 alias is='roslaunch image_shrinker image_shrinker.launch'
+
 
 # computing nodes
 alias gmapp='roslaunch '$HELPER_PKG' slam_gmapping_haw.launch'
@@ -36,6 +40,7 @@ alias camera_small_theora='rosrun image_view image_view image:=/camera/image_sma
 alias kr='rosrun teleop_twist_keyboard teleop_twist_keyboard.py'
 alias kra='rosrun teleop_twist_keyboard teleop_twist_keyboard_arm_cam.py'
 
+
 # small tools
 alias rka='rosnode list; rosnode kill -a; rosnode list'
 alias rnl='rosnode list'
@@ -43,4 +48,4 @@ alias rnl='rosnode list'
 alias joint_states='rostopic echo -n 1 /schunk/position/joint_states'
 alias joint_movePos='rostopic pub -1 /movePosition metralabs_ros/idAndFloat -- '
 alias joint_targetVel='rostopic pub -1 /targetVelocity metralabs_ros/idAndFloat -- '
-
+alias joint_ref_gripper='rostopic pub -1 /ref std_msgs/Int8 5'
