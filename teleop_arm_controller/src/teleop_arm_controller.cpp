2511b9268374640d0f72b585ac7efbb4f16eb600
Ben Struss <ben@just-b.org>
2012-04-23 12:33:11 +0200

    Tested and working BUG: single-tapping sometimes leads to continous movement of arm

---
+++ teleop_arm_controller/src/teleop_arm_controller.cpp
@@ -0,0 +1,218 @@
+#include <cmath>
+
+#include <ros/ros.h>
+
+#include <metralabs_ros/idAndFloat.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Twist.h>
+
+#define SLEEP_TIME_SECS 0.10
+#define TIMEOUT_SECS    0.25
+
+#define YAW_JOINT_ID    0
+#define PITCH_JOINT_ID  3
+
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
+
+
+ros::Publisher pubMoveVel;
+ros::Time lastMessage;
+sensor_msgs::JointStateConstPtr lastJointState;
+bool initiatedMovement;
+
+bool checkVelocityValid(int joint, double velocity, double currentAngle)
+{
+  if (joint == YAW_JOINT_ID)
+  {
+    if ((velocity > 1e-6 && currentAngle >= 170*(M_PI/180)) || (velocity < -1e-6 && currentAngle <= -170*(M_PI/180)))
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+  else if (joint == PITCH_JOINT_ID)
+  {
+    if ((velocity > 1e-6 && currentAngle >= 50*(M_PI/180)) || (velocity < -1e-6 && currentAngle <= -120*(M_PI/180)))
+    {
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void sendVelocity(int joint, double velocity)
+{
+  metralabs_ros::idAndFloatPtr output = boost::make_shared<metralabs_ros::idAndFloat>();
+  output->id = joint;
+  output->value = velocity;
+  pubMoveVel.publish(output);
+}
+
+void jointStatesCallback(const sensor_msgs::JointStateConstPtr& newState)
+{
+  lastJointState = newState;
+  if (initiatedMovement)
+  {
+    ROS_INFO("Got joint states and i am moving the arm");
+    if (!checkVelocityValid(YAW_JOINT_ID, lastJointState->velocity.at(YAW_JOINT_ID), lastJointState->position.at(YAW_JOINT_ID)))
+    {
+      sendVelocity(YAW_JOINT_ID, 0);
+    }
+    if (!checkVelocityValid(PITCH_JOINT_ID, lastJointState->velocity.at(PITCH_JOINT_ID), lastJointState->position.at(PITCH_JOINT_ID)))
+    {
+      sendVelocity(PITCH_JOINT_ID, 0);
+    }
+  }
+  else
+  {
+    ROS_INFO("Got joint states and i am NOT moving the arm");
+  }
+}
+
+void moveVelocityCallback(const geometry_msgs::TwistConstPtr& moveVelocity)
+{
+  lastMessage = ros::Time::now();
+  double yaw = MAX(MIN(moveVelocity->angular.z, 0.2), -0.2);
+  double pitch = MAX(MIN(moveVelocity->angular.y, 0.2), -0.2);
+
+  bool moving = false;
+
+  ROS_INFO("Yaw: %f  Pos: %f  CurVel: %f", yaw, lastJointState->position.at(YAW_JOINT_ID), lastJointState->velocity.at(YAW_JOINT_ID));
+  if (std::abs(yaw) > 1e-4 && checkVelocityValid(YAW_JOINT_ID, yaw, lastJointState->position.at(YAW_JOINT_ID)))
+  {
+    if (std::abs(yaw - lastJointState->velocity.at(YAW_JOINT_ID)) > 2e-2)
+    {
+      sendVelocity(YAW_JOINT_ID, yaw);
+      ROS_INFO("Started yaw movement: %f", yaw);
+    }
+    moving = true;
+  }
+  else
+  {
+    if (std::abs(lastJointState->velocity.at(YAW_JOINT_ID)) > 1e-4)
+    {
+      sendVelocity(YAW_JOINT_ID, 0);
+    }
+  }
+
+  ROS_INFO("Pitch: %f  Pos: %f  CurVel: %f", pitch, lastJointState->position.at(PITCH_JOINT_ID), lastJointState->velocity.at(PITCH_JOINT_ID));
+  if (std::abs(pitch) > 1e-6 && checkVelocityValid(PITCH_JOINT_ID, pitch, lastJointState->position.at(PITCH_JOINT_ID)))
+  {
+    if (std::abs(pitch - lastJointState->velocity.at(PITCH_JOINT_ID)) > 2e-2)
+    {
+      sendVelocity(PITCH_JOINT_ID, pitch);
+      ROS_INFO("Started pitch movement: %f", pitch);
+    }
+    moving = true;
+  }
+  else
+  {
+    if (std::abs(lastJointState->velocity.at(PITCH_JOINT_ID)) > 1e-4)
+    {
+      sendVelocity(PITCH_JOINT_ID, 0);
+    }
+  }
+  initiatedMovement = moving;
+}
+
+
+//bool serviceCallback( joint_motion_service::move_joints_service::Request &request, joint_motion_service::move_joints_service::Response &response)
+//{
+//        ROS_INFO("Service called!");
+//
+//        if(lastJointState == 0) {
+//                ROS_ERROR("Joint move service receives no joint states!");
+//                response.positions_ok = false;
+//                return true;
+//        }
+//
+//        // send wanted positions
+//        for (uint joint = 0; joint < request.joint_ids.size(); ++joint) {
+//                metralabs_ros::idAndFloatPtr output = boost::make_shared<metralabs_ros::idAndFloat>();
+//                output->id = request.joint_ids.at(joint);
+//                output->value = request.positions.at(joint);
+//                pubMoveVel.publish(output);
+//
+//                ros::Duration(SLEEP_TIME_SECS).sleep();
+//        }
+//
+//        // wait for joints to reach position
+//        ros::Time timeoutTime = ros::Time::now() + ros::Duration(TIMEOUT_SECS);
+//        bool allPositionsReached;
+//        do {
+//                // check for timeout, arm to slow or not moving anymore
+//                if(ros::Time::now() > timeoutTime) {
+//                        ROS_ERROR("Move joints service timed out! Stopping joint.");
+//                        response.positions_ok = false;
+//                        return true;
+//                }
+//
+//                // sleep while arm moves a bit
+//                ros::Duration(SLEEP_TIME_SECS).sleep();
+//
+//                // check if shutdown requested (e.g. Ctrl-C)
+//                if(ros::isShuttingDown())
+//                        return false;
+//
+//                // let the newest joint state message to be received
+//                ros::spinOnce();
+//
+//                // check all current and wanted joint positions
+//                allPositionsReached = true;
+//                for (uint i = 0; i < request.joint_ids.size(); ++i) {
+//                        uint id = request.joint_ids.at(i);
+//                        float wishPosition = request.positions.at(i);
+//                        float lastPosition = lastJointState->position.at(id);
+//                        float deltaPosition = wishPosition - lastPosition;
+//                        ROS_DEBUG("delta=%f", deltaPosition);
+//                        if(fabs(deltaPosition) > EPSILON_RADIAN) {
+//                                allPositionsReached = false;
+//                                break;
+//                        }
+//                }
+//        } while(!allPositionsReached);
+//
+//        response.positions_ok = true;
+//        ROS_INFO("Service done!");
+//        return true;
+//}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "teleop_arm_controller");
+  ros::NodeHandle nh;
+
+  pubMoveVel = nh.advertise<metralabs_ros::idAndFloat>("/moveVelocity", 1, false);
+  ros::Subscriber subJointStates = nh.subscribe("/schunk/position/pre_mimic/joint_states", 1, jointStatesCallback);
+  ros::Subscriber subArmTwist = nh.subscribe("/moveArmVelocity", 1, moveVelocityCallback);
+
+  ROS_INFO("Teleop arm controller online.");
+  while (ros::ok())
+  {
+    ros::spinOnce();
+    if (initiatedMovement)
+    {
+      if (ros::Time::now() > lastMessage + ros::Duration(TIMEOUT_SECS))
+      {
+        // no message received for the timeout period. stopping motion
+        sendVelocity(YAW_JOINT_ID, 0);
+        sendVelocity(PITCH_JOINT_ID, 0);
+        initiatedMovement = false;
+        ROS_INFO("Message timeout. Motion halted!");
+      }
+    }
+    ros::Duration(SLEEP_TIME_SECS).sleep();
+  }
+  return 0;
+}
