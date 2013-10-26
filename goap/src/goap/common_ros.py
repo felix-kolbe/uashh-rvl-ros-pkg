6554d09ffc5492acd28241ceb2139e3a52955cec
Felix Kolbe <felix.kolbe@informatik.haw-hamburg.de>
2013-10-27 01:34:21 +0200

    Add MoveBaseAction.CheckForPathVarEffect to check path availability using move_base server

--- goap/src/goap/common_ros.py
+++ goap/src/goap/common_ros.py
@@ -13,6 +13,9 @@ import tf
 from uashh_smach.platform.move_base import MoveBaseState, pose_orientation_to_quaternion
 
 from std_msgs.msg import Empty
+from nav_msgs.msg import Path
+from nav_msgs.srv import GetPlan, GetPlanRequest
+from geometry_msgs.msg import PoseStamped
 
 from common import Action, Condition, Precondition, Effect, VariableEffect
 
@@ -71,16 +74,47 @@ class ResetBumperAction(Action):
 
 class MoveBaseAction(SMACHStateWrapperAction):
 
+    class CheckForPathVarEffect(VariableEffect):
+        def __init__(self, condition):
+            VariableEffect.__init__(self, condition)
+            self.service_topic = '/move_base/make_plan'
+            self._service_proxy = rospy.ServiceProxy(self.service_topic, GetPlan)
+            self._planned_paths_pub = rospy.Publisher('/task_planning/goal_paths', Path)
+
+        def _is_reachable(self, value, start_value):
+            request = GetPlanRequest()
+            request.start.header.frame_id = '/map'
+            request.start.pose = start_value
+            request.goal.header.frame_id = '/map'
+            request.goal.pose = value
+            request.tolerance = 0.2 # meters in x/y
+            response = self._service_proxy(request)
+            self._planned_paths_pub.publish(response.plan)
+            return len(response.plan.poses) > 0
+
     def __init__(self):
         self._condition = Condition.get('robot.pose')
+        self._check_path_vareffect = MoveBaseAction.CheckForPathVarEffect(self._condition)
         SMACHStateWrapperAction.__init__(self, MoveBaseState(),
                         [Precondition(Condition.get('robot.bumpered'), False),
                          Precondition(Condition.get('robot.arm_folded'), True)],
-                        [VariableEffect(self._condition)])
+                        [self._check_path_vareffect])
 
     def check_freeform_context(self):
         # TODO: cache freeform context?
-        return self.state._action_client.wait_for_server(rospy.Duration(0.1))
+        if not self.state._action_client.wait_for_server(rospy.Duration(1)):
+            rospy.logwarn("%s context check: cannot access move_base action server"
+                          % self.__class__.__name__)
+            return False
+        try:
+            self._check_path_vareffect._service_proxy.wait_for_service(1)
+        except rospy.exceptions.ROSException:
+            rospy.logwarn("%s context check: cannot access %s service server"
+                          % (self.__class__.__name__,
+                             self._check_path_vareffect.service_topic))
+            return False
+        return True
+
 
     def apply_adhoc_preconditions_for_vareffects(self, var_effects, worldstate, start_worldstate):
         effect = var_effects.pop()  # this action has one variable effect
